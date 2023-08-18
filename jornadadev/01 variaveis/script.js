857811df528b9edd19e43a4d4ede0aa1a506ab7d
LucasPess <lucas82930@gmail.com>
2023-08-18 13:57:29 -0300

    ...

---
+++ jornadadev/01 variaveis/script.js
@@ -0,0 +1,44 @@
+//DECLARANDO VARIAVEL
+let nomeDaVariavel = 9;
+let fruta ='morango';
+
+//TIPOS DE DADOS EM VARIAVEIS
+
+// String
+
+let nome = 'Lucas';
+
+//Integers
+
+let idade =24;
+
+//Float
+
+let nota = 7.2;
+
+//Boleano
+//true
+//false
+
+let lucasEhRuivo = false;
+
+//Arrays
+//contem diversos valores
+
+let frutas = ['maçã','banana','laranja'];
+
+frutas[0];
+
+//Objetos
+
+let xicara = { cor:'branca', material: 'porcelana', peso: 100}; //100g
+
+
+//DESAFIO
+let fiatUno = {
+    ano: 2021,
+    modelo: 'way',
+    foraDeLinha:false,
+    cor:['preto','branco','cinza']
+};
+
