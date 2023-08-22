45950fd8270614cee8dc3bb7e0284eb11f7401b2
Lucas Emanuel T. Pessoa <lucas82930@gmail.com>
2023-08-22 12:26:15 -0300

    strings

---
+++ jornadadev/strings.js
@@ -0,0 +1,19 @@
+// Como declarar uma variavel do tipo string
+
+let string = 'texto';
+
+// Concatenar strings
+
+let msg1 = 'olá, ';
+let msg2 = 'seja bem vindo!';
+let concatenar = msg1 + msg2;
+
+let pessoa = 'Lucas';
+let msg3 = ` ${pessoa} olá, seja bem vindo(a)!`;
+
+console.log(msg3);
+
+// Exercicio
+
+let usuario = prompt('Qual o seu nome? ');
+console.log('Olá, ' + usuario + ' seja bem vindo!')
