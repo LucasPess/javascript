06d19faf9bdcda143dc620bcf5d40ed1cf05902b
Lucas Emanuel T. Pessoa <lucas82930@gmail.com>
2023-08-22 13:41:20 -0300

    arrays

---
+++ jornadadev/arrays.js
@@ -0,0 +1,8 @@
+let cargos = [ 'Administrador','Médico', 'Advogado', 'Analista de sistemas'];
+
+cargos[1] = 'Contador';
+
+cargos.push('Devops') // Adciona item a lista de arrays
+cargos.pop() // Retira o ultimo item da lista de arrays
+
+console.log(cargos);
