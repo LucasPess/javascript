9b89a567209bdd32be08a8ca73df18625433cea3
Lucas Emanuel T. Pessoa <lucas82930@gmail.com>
2023-08-22 16:13:59 -0300

    operadores aritimeticos

---
+++ jornadadev/operadoresAritimeticos.js
@@ -0,0 +1,24 @@
+// Somar
+10 + 10
+
+// Subtrair
+10 - 5
+
+// Multiplicar
+10 * 5
+
+// Dividir
+5 / 10
+
+let calculo = 10;
+let calculo2 = 5;
+
+let multiplicarCalculo = calculo * calculo2;
+
+// Desafio 
+let p1 = 20;
+let p2 = 20;
+let amigos = 5;
+
+let divs = (p1 + p2) / amigos
+console.log(divs)
