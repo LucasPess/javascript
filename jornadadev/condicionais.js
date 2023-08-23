3898dbcbc13cc1c5c445b13132ffee1197ee429d
Lucas Emanuel T. Pessoa <lucas82930@gmail.com>
2023-08-23 10:42:24 -0300

    condicionais

---
+++ jornadadev/condicionais.js
@@ -0,0 +1,11 @@
+let nota1 = parseFloat(prompt('Digite sua nota da prova:'));
+let nota2 = parseFloat(prompt('Digite sua outra nota da prova:'));
+let mediaNota = (parseFloat(nota1) + parseFloat(nota2)) / 2;
+
+if(mediaNota >= 5){
+    console.log('Sua media foi '+ mediaNota +', você passou!');
+}else{
+    console.log('Sua media foi '+ mediaNota +' você reprovou!');
+}
+
+// só fiz colar meu deafio kkkkkkkkkkkkkkkkkkk
