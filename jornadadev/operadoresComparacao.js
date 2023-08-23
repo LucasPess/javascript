a0d8593439288f79525f69b6d787e86660fb7c6d
Lucas Emanuel T. Pessoa <lucas82930@gmail.com>
2023-08-23 10:15:30 -0300

    operadores de comparação

---
+++ jornadadev/operadoresComparacao.js
@@ -0,0 +1,18 @@
+// Comparar se é identico a alguma coisa
+a === b;
+
+// Comparar se não é identico a alguma coisa
+a !== b;
+
+// Maior Que
+20 > 10;
+
+//Menor Que 
+10 < 20;
+
+// Maior ou iagual a
+20 >= 10;
+
+// Menor ou igual a 
+10 <= 20;
+
