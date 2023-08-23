c103176213bfd5d0222b5b88068e747355dd9bae
Lucas Emanuel T. Pessoa <lucas82930@gmail.com>
2023-08-23 10:38:45 -0300

    operadores de comparação

--- jornadadev/operadoresComparacao.js
+++ jornadadev/operadoresComparacao.js
@@ -1,8 +1,8 @@
 // Comparar se é identico a alguma coisa
-a === b;
+'a' === 'b';
 
 // Comparar se não é identico a alguma coisa
-a !== b;
+'a' !== 'b';
 
 // Maior Que
 20 > 10;
@@ -16,3 +16,14 @@ a !== b;
 // Menor ou igual a 
 10 <= 20;
 
+
+// Desafio
+let nota1 = parseFloat(prompt('Digite sua nota da prova:'));
+let nota2 = parseFloat(prompt('Digite sua outra nota da prova:'));
+let mediaNota = (parseFloat(nota1) + parseFloat(nota2)) / 2;
+
+if(mediaNota >= 5){
+    console.log('Sua media foi '+ mediaNota +', você passou!');
+}else{
+    console.log('Sua media foi '+ mediaNota +' você reprovou!');
+}
