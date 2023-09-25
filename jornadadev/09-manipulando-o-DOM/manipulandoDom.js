6fca7bafc909c9d805b0002b30a95666a83e40fd
Lucas Emanuel T. Pessoa <lucas82930@gmail.com>
2023-09-25 14:33:02 -0300

    Dom

---
+++ jornadadev/09-manipulando-o-DOM/manipulandoDom.js
@@ -0,0 +1,25 @@
+// Capturando um elemento por ID
+
+//let titulo = document.getElementById("titulo-1");
+
+// Capturando por class
+
+//let titulo2 = document.getElementsByClassName("titulo-2");
+
+// Capturando por tags
+
+//let tags = document.getElementsByTagName("h1");
+
+//let novoTitulo = document.getElementById("nome").innerHTML = prompt("Digite seu nome: ");
+
+//let msg = prompt("deseja ver uma mensagem escondida ? (digite 1 para sim e 2 para não)")
+
+//if(msg === "1"){
+//    let novoElemento = document.createElement("h2");
+//    novoElemento.innerHTML = "elemento h2";
+//    document.body.appendChild(novoElemento);
+//}
+
+let removerNome = document.getElementById('nome');
+removerNome.remove()
+
