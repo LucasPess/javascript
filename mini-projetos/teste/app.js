e29452eb5d7f89ea6c633b95bbaf1b2aac6fcca4
Lucas Emanuel T. Pessoa <lucas82930@gmail.com>
2023-09-27 17:05:26 -0300

    testando

---
+++ mini-projetos/teste/app.js
@@ -0,0 +1,18 @@
+let cor = document.getElementById('titulo')
+let cores = ['red', 'blue', 'yellow','green'];
+
+function mudarCor(){
+    if(document.body.style.backgroundColor == cores[0]){
+        document.body.style.backgroundColor = cores[1];
+        cor.innerHTML = `Cor de fundo:  ${cores[1]}`
+        } else if(document.body.style.backgroundColor == cores[1]){
+            document.body.style.backgroundColor = cores[2];
+            cor.innerHTML = `Cor de fundo:  ${cores[2]}`
+        } else if(document.body.style.backgroundColor == cores[2]){
+            document.body.style.backgroundColor = cores[3];
+            cor.innerHTML = `Cor de fundo:  ${cores[3]}`
+        } else{
+            document.body.style.backgroundColor =cores[0]
+            cor.innerHTML = `Cor de fundo:  ${cores[0]}`
+        }
+}
