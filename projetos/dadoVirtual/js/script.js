22dcebc885c4eadbd321316603086993211af561
Lucas Emanuel T. Pessoa <lucas82930@gmail.com>
2023-12-14 16:07:27 -0300

    dado virtual

---
+++ projetos/dadoVirtual/js/script.js
@@ -0,0 +1,38 @@
+let numeroSorteado = 0;
+let imgDado = document.querySelector('#imgDado');
+let btnSortear = document.querySelector('#btnSortear');
+let sorteado = document.querySelector('#sorteado');
+let dadoRolando = document.querySelector('#dadoRolando');
+
+
+btnSortear.addEventListener('click', function(){
+    imgDado.classList.add('animar');
+    sorteado.classList.add('aparecer');
+
+    dadoRolando.play();
+
+    btnSortear.style.display = 'none';
+
+    setTimeout(function() {
+
+        numeroSorteado = getRandomInt(1,6);
+        console.log(numeroSorteado);
+
+        imgDado.setAttribute('src', '../medias/dado/'+numeroSorteado+'.png');
+
+        sorteado.textContent = numeroSorteado;
+
+        btnSortear.style.display = 'inline-block';
+
+        imgDado.classList.remove('animar');
+        sorteado.classList.remove('aparecer');
+    }, 1750)
+
+});
+
+function getRandomInt(min,max){
+    min = Math.ceil(min); //arredondar para cima ceil = teto
+    max = Math.floor(max); //arredondar para baixa floor = piso
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+
+}
