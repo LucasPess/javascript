077cc181232ddc8de8b7055bed97411c41c8cad1
Lucas Emanuel T. Pessoa <lucas82930@gmail.com>
2024-01-22 14:56:46 -0300

    finalizando projeto

--- projetos/drum/script.js
+++ projetos/drum/script.js
@@ -5,7 +5,10 @@ document.body.addEventListener('keyup', (event) =>{
 document.querySelector('.composer button').addEventListener('click',() => {
     let song = document.querySelector('#input').value;
 
-    console.log("Musica", song);
+    if(song !== ''){
+        let songArray = song.split('');
+        playComposition(songArray);
+    }
 });
 
 function playSound(sound){
@@ -23,5 +26,14 @@ function playSound(sound){
         setTimeout(() =>{
             keyElement.classList.remove('active');
         }, 300);
+    }
+}
+
+function playComposition(songArray){
+    for(let songItem of songArray) {
+        setTimeout(()=>{
+            playSound(`key${songItem}`);
+        }, wait);
+        wait += 250;
     }
 }
